1e24f1c19c15225db9f90ba1cdf3497864f3cdcf
InCogNiTo124 <InCogNiTo124>
2022-12-06 18:35:50 +0100

    day5, part1

---
+++ 05.py
@@ -0,0 +1,38 @@
+import sys
+from collections import deque
+from string import ascii_uppercase
+import re
+
+UPPER = set(ascii_uppercase)
+PATTERN = re.compile(r'move (\d+) from (\d+) to (\d+)')
+
+stacks = []
+
+def parse(match):
+    return int(match.group(1)), int(match.group(2))-1, int(match.group(3))-1
+
+# crates parsing
+for line in sys.stdin:
+    line = line.rstrip()
+    if line == '':
+        break
+    line = line.replace('[', ' ').replace(']', ' ')
+    crates = line[1::4]
+    if len(stacks) == 0:
+        for _ in range(len(crates)):
+            stacks.append(deque())
+    for i, t in enumerate(crates):
+        if t in UPPER:
+            stacks[i].appendleft(t)
+
+for line in sys.stdin:
+    line = line.rstrip()
+    if line == '':
+        break
+    match = PATTERN.match(line)
+    count, src, dest = parse(match)
+    for _ in range(count):
+        stacks[dest].append(stacks[src].pop())
+
+print(''.join(t[-1] for t in stacks))
+
